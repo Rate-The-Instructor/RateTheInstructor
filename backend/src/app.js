c3603b075c0e4a07e27934f2176564798ff6b60c
Merwan-J <merwanj71@gmail.com>
2023-01-11 10:08:21 +0300

    file setup

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+import express, {
+  Application,
+  Request,
+  Response,
+  NextFunction,
+  json,
+} from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+export default app;
