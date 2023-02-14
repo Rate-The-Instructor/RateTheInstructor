e5438fd8d9c7b598a817ebffce5ea2c9fc684975
tura-lab <tiruzertsedeke25@gmail.com>
2023-02-15 00:23:33 +0300

    done instructor review component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InstructorReviewComponent } from './components/instructor-review/instructor-review.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'rate/:instructorId', component: InstructorReviewComponent },
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SignupComponent } from './components/signup/signup.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RatingSystemComponent } from './components/rating-system/rating-system.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Components
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { LoginComponent } from './components/login/login.component';
+import { InstructorReviewComponent } from './components/instructor-review/instructor-review.component';
 
-
-// Angular material stuff
+// Angular-material stuff
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
@@ -24,6 +25,8 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
+
 
 
 @NgModule({
@@ -40,16 +43,15 @@ import {MatMenuModule} from '@angular/material/menu';
     MatNativeDateModule,
     MatInputModule,
     MatSelectModule,
-    
     MatIconModule,
-    
     MatRadioModule,
     MatGridListModule,
     MatToolbarModule,
     MatMenuModule,
-    MatButtonModule  
+    MatButtonModule,
+    MatCardModule
   ],
-  declarations: [AppComponent, SignupComponent, RatingSystemComponent, LoginComponent, NavBarComponent],
+  declarations: [AppComponent, SignupComponent, RatingSystemComponent, LoginComponent, NavBarComponent, InstructorReviewComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
