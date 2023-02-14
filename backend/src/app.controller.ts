d0f181bd5d916b430c670fc649f223cab7055e3f
Merwan-J <merwanj71@gmail.com>
2023-02-14 22:13:04 +0300

    reverted back to nest

---
+++ backend/src/app.controller.ts
@@ -0,0 +1,8 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+}
