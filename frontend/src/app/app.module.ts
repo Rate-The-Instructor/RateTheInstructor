48c6526559f7e5428f6ab5db2c3f0832aa5515c3
tura-lab <tiruzertsedeke25@gmail.com>
2023-02-15 04:21:18 +0300

    review page done

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,14 +5,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RatingSystemComponent } from './components/rating-system/rating-system.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
 // Components
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { LoginComponent } from './components/login/login.component';
 import { InstructorReviewComponent } from './components/instructor-review/instructor-review.component';
+import { InstructorReviewInputComponent } from './components/instructor-review-input/instructor-review-input.component';
 
 // Angular-material stuff
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -24,10 +25,10 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatCardModule} from '@angular/material/card';
-
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { TopTagsComponent } from './components/top-tags/top-tags.component';
 
 @NgModule({
   imports: [
@@ -49,9 +50,19 @@ import {MatCardModule} from '@angular/material/card';
     MatToolbarModule,
     MatMenuModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatChipsModule,
   ],
-  declarations: [AppComponent, SignupComponent, RatingSystemComponent, LoginComponent, NavBarComponent, InstructorReviewComponent],
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    RatingSystemComponent,
+    LoginComponent,
+    NavBarComponent,
+    InstructorReviewComponent,
+    InstructorReviewInputComponent,
+    TopTagsComponent
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
