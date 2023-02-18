549f4aa5458ae92447d578f1b1945d971de6df88
Merwan-J <merwanj71@gmail.com>
2023-02-18 22:27:11 +0300

    [backend] branch ready for deployment

--- frontend/src/app/components/top-tags/top-tags.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-top-tags',
-  templateUrl: './top-tags.component.html',
-  styleUrls: ['./top-tags.component.css'],
-})
-export class TopTagsComponent {}
