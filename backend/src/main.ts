f3ea4e20264f3e843448ff5e700f176fb33dd4eb
Merwan Juneydi <76240948+Merwan-J@users.noreply.github.com>
2023-02-19 15:55:16 +0300

    [backend] deployment issues fixed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
