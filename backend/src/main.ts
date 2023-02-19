5b7bebe6ab6df1a666e941d72c5bc5a01b5221a9
Merwan-J <merwanj71@gmail.com>
2023-02-19 14:25:47 +0300

    Co-authored-by: NAME amanuelyihunie27@gmail.com

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   await app.listen(3000);
